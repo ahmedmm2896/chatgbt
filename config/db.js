51400c065b88a6f69cfde9e89658844f7b869598
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-04 23:38:01 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // Replace <db_password> with your actual password and 'myDatabaseName' with the name of your database
-    await mongoose.connect('mongodb+srv://ahmuhanna2896:cotJjkPIo36BKX7Z@cluster0.5lb0b.mongodb.net/ChatGbt.ChatGBT
-?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://ahmuhanna2896:cotJjkPIo36BKX7Z@cluster0.5lb0b.mongodb.net/ChatGbt?retryWrites=true&w=majority');
     console.log('MongoDB connected...');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
