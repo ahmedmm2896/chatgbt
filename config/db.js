a7899c7295c25582eb09b961f4f254db8ce45e62
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-04 23:56:48 +0300

    Added user authentication and installed new packages

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    // Replace <db_password> with your actual password and 'myDatabaseName' with the name of your database
+    await mongoose.connect('mongodb+srv://ahmuhanna2896:cotJjkPIo36BKX7Z@cluster0.5lb0b.mongodb.net/ChatGbt?retryWrites=true&w=majority');
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error('MongoDB connection failed:', err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
