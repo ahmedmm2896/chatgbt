93bdee5cab18014cbf80222d8464662ee2e80d32
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-04 22:59:05 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ahmuhanna2896:<db_password>@cluster0.5lb0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    await mongoose.connect('mongodb+srv://ahmuhanna2896:<cotJjkPIo36BKX7Z>@cluster0.5lb0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
