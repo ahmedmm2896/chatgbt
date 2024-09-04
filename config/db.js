4e8fb2a42bc6b3c200629e7cf94c5c89233e5652
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-04 23:28:33 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ahmuhanna2896:<cotJjkPIo36BKX7Z>@cluster0.5lb0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Replace <db_password> with your actual password and 'myDatabaseName' with the name of your database
+    await mongoose.connect('mongodb+srv://ahmuhanna2896:<cotJjkPIo36BKX7Z>@cluster0.5lb0b.mongodb.net/myDatabaseName?retryWrites=true&w=majority');
     console.log('MongoDB connected...');
   } catch (err) {
-    console.error(err.message);
+    console.error('MongoDB connection failed:', err.message);
     process.exit(1); // Exit process with failure
   }
 };
