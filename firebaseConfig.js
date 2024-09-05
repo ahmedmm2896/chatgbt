90a009e534b83718cf265ab4835e8a47146eaf14
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-05 17:48:57 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,18 @@
+// firebaseConfig.js
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// Firebase config object from your Firebase console
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
