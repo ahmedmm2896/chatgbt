1c07d2b494d8a5baf8024210474b4b4fb6bdf370
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-05 15:24:58 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// Define the user schema
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -14,16 +13,13 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-// Password hashing before saving the user
 UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-// Compare password for login validation
 UserSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
