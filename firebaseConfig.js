128877fd998fd6a6371b725ade2b5cc81a9c8810
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-05 18:13:20 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebaseConfig.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Import the Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyCN0cGJ7Brza_GUJiFIzFURaeis5Ykyr2c",
-  authDomain: "pharmai-d81af.firebaseapp.com",
-  projectId: "pharmai-d81af",
-  storageBucket: "pharmai-d81af.appspot.com",
-  messagingSenderId: "332997353967",
-  appId: "1:332997353967:web:6e9d25d1387a470584961a",
-  measurementId: "G-N0BD753TYM"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app);
+
+export { auth };
