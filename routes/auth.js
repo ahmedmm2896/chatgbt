08ee3eba1576bd94e23752db8a2d698cba0d2198
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-05 15:24:37 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth'); // Use the controllers for logic
-const authMiddleware = require('../middleware/auth'); // Import the auth middleware
+const authController = require('../controllers/auth');
+const authMiddleware = require('../middleware/auth');
 
 // Sign-up route
-router.post('/signup', authController.register);
+router.post('/signup', authController.signup);
 
 // Login route
 router.post('/login', authController.login);
 
 // Get the logged-in user
 router.get('/user', authMiddleware, authController.getUser);
-
-// Example protected route (for testing the middleware)
-router.get('/profile', authMiddleware, (req, res) => {
-  res.send('Welcome to your profile');
-});
 
 module.exports = router;
