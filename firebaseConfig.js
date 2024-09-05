7376fbbddfb90dbeaf951106e040caeb7be5dd83
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-05 23:42:19 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: AIzaSyCN0cGJ7Brza_GUJiFIzFURaeis5Ykyr2c,
+    authDomain: d81af.firebaseapp.com,
+    projectId: pharmai-d81af,
+    storageBucket: pharmai-d81af.appspot.com,
+    messagingSenderId: 332997353967,
+    appId: 1:332997353967:web:6e9d25d1387a470584961a
 };
 
 // Initialize Firebase
