a32fdd706b3f02b5e0b2b4245ad33411b43369fc
ahmedmm2896 <ahmuhanna2896@gmail.com>
2024-09-07 16:27:31 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,20 +3,19 @@ let auth0 = null;
 const configureClient = async () => {
   try {
     const response = await fetch("/auth_config.json");
-    
-    // Check if the response is OK
+
     if (!response.ok) {
       throw new Error(`Could not fetch auth_config.json: ${response.statusText}`);
     }
-    
+
     const config = await response.json();
 
     auth0 = await createAuth0Client({
       domain: "pharmai.us.auth0.com",
       client_id: "tQtgm50zwKorzoDwVbqOzXq50V5DBMfx",
-      redirect_uri: window.location.origin + "/dashboard.html",  // This should match your Auth0 settings
+      redirect_uri: window.location.origin + "/dashboard.html",
     });
-    
+
   } catch (err) {
     console.error("Error configuring Auth0 client:", err.message);
   }
@@ -40,12 +39,14 @@ window.onload = async () => {
     }
   }
 
+  // Check if the user is authenticated
   const isAuthenticated = await auth0.isAuthenticated();
 
   if (isAuthenticated) {
-    document.getElementById("loginBtn").style.display = "none";
-    document.getElementById("dashboardBtn").style.display = "block";
+    // The user is authenticated, redirect them to the dashboard
+    window.location.href = '/dashboard.html';
   } else {
+    // The user is not authenticated, show the login button
     document.getElementById("loginBtn").style.display = "block";
     document.getElementById("dashboardBtn").style.display = "none";
   }
@@ -66,7 +67,7 @@ const login = async () => {
 const logout = () => {
   if (auth0) {
     auth0.logout({
-      returnTo: window.location.origin, // You can change this to your desired return URL
+      returnTo: window.location.origin,
     });
   } else {
     console.error("Auth0 client is not initialized.");
